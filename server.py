3fe9f4f9233bed9077efad9fa1dbe8a591daa135
aurelien.fache <aurelien.fache@saflab.io>
2023-11-18 09:47:38 +0100

    Add functionality to save train speed to file

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 
 @app.route("/api_train", methods=["GET"])
 def get_vitesse():
+    """
+    Get the train speed from the request parameters and save it to a file.
+
+    Returns:
+        str: A message indicating that the speed has been saved to the file.
+    """
     vitesse = request.args.get("vitesse")
     if vitesse is not None:
         with open("speed.txt", "w") as f:
